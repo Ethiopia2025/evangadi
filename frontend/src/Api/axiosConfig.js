72ecebf905b1637009644ac66f37f55190219f3d
Yosef <woldeyosef141@gmail.com>
2025-09-24 19:31:55 +0300

    update backend

--- frontend/src/Api/axiosConfig.js
+++ frontend/src/Api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api", // ✅ correct
+  // baseURL: "http://localhost:8000/api", 
+  baseURL: "https://evangadi06.onrender.com/api",
 });
 
 export default axiosInstance;
